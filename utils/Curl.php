bff2fd4bb0eb4389f5334b7b0ac91e1e25f52b30
sunnnnn <263021000@qq.com>
2017-07-01 10:40:30 +0800

    curl

--- utils/Curl.php
+++ utils/Curl.php
@@ -2,116 +2,135 @@
 namespace sunnnnn\helper\utils;
 
 class Curl{
-	
-	public function get($url, $httpHeader = [], $header = false){
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_HEADER, $header);
-		if(!empty($httpHeader) && is_array($httpHeader)){
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $httpHeader);
-		}
-		curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-		$result = curl_exec($ch);
-		if (curl_errno($ch)) {
-			return json_encode(['error' => curl_error($ch)]);
-		}
-		curl_close($ch);
-		return $result;
-	}
-	
-	public function post($url, $data, $httpHeader = [], $header = false){
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_POST, true);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-		curl_setopt($ch, CURLOPT_HEADER, $header);
-		if(!empty($httpHeader) && is_array($httpHeader)){
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $httpHeader);
-		}
-		curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-		$result = curl_exec($ch);
-		if (curl_errno($ch)) {
-			return json_encode(['error' => curl_error($ch)]);
-		}
-		curl_close($ch);
-		return $result;
-	}
-	
-	public function put($url, $data, $header = false){
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-		curl_setopt($ch, CURLOPT_HEADER, $header);
-		curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, ["X-HTTP-Method-Override: put"]);
-		$result = curl_exec($ch);
-		if (curl_errno($ch)) {
-			return json_encode(['error' => curl_error($ch)]);
-		}
-		curl_close($ch);
-		return $result;
-	}
-	
-	public function delete($url, $data, $header = false){
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-		curl_setopt($ch, CURLOPT_HEADER, $header);
-		curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, ["X-HTTP-Method-Override: delete"]);
-		$result = curl_exec($ch);
-		if (curl_errno($ch)) {
-			return json_encode(['error' => curl_error($ch)]);
-		}
-		curl_close($ch);
-		return $result;
-	}
-	
-	public function request($url, $method, $data, $header = false, $headers = [], $cert = []){
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_HEADER, $header);
-		curl_setopt($ch, CURLOPT_TIMEOUT,30);
-		switch($method){
-			case 'post': 
-				curl_setopt($ch, CURLOPT_POST, true);
-				curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-				break;
-			case 'put':
-				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
-				curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-				curl_setopt($ch, CURLOPT_HTTPHEADER, ["X-HTTP-Method-Override: put"]);
-				break;
-			case 'delete':
-				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
-				curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-				curl_setopt($ch, CURLOPT_HTTPHEADER, ["X-HTTP-Method-Override: delete"]);
-				break;
-			default: break;
-		}
-// 		curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,false);
-// 		curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,false);
-// 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-// 		curl_setopt($ch, CURLOPT_AUTOREFERER, true);
-		if(!empty($headers) && is_array($headers)) curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-		if(!empty($cert) && is_array($cert)){
-			curl_setopt($ch, CURLOPT_SSLCERT, $cert['cert']);
-			curl_setopt($ch, CURLOPT_SSLKEY,  $cert['key']);
-		}
-		
-		$result = curl_exec($ch);
-		if (curl_errno($ch)) {
-			return json_encode(['error' => curl_error($ch)]);
-		}
-		curl_close($ch);
-		return $result;
-	}
-	
+    
+    public function get($url, $options = []){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+        
+        if(!empty($options) && is_array($options)){
+            foreach($options as $curlopt => $value){
+                curl_setopt($ch, $curlopt, $value);
+            }
+        }
+        
+        $result = curl_exec($ch);
+        if (curl_errno($ch)) {
+            return json_encode(['error' => curl_error($ch)]);
+        }
+        curl_close($ch);
+        return $result;
+    }
+    
+    public function post($url, $data, $options= []){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        
+        if(!empty($options) && is_array($options)){
+            foreach($options as $curlopt => $value){
+                curl_setopt($ch, $curlopt, $value);
+            }
+        }
+        
+        $result = curl_exec($ch);
+        if (curl_errno($ch)) {
+            return json_encode(['error' => curl_error($ch)]);
+        }
+        curl_close($ch);
+        return $result;
+    }
+    
+    public function put($url, $data, $options= []){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ["X-HTTP-Method-Override: put"]);
+        
+        if(!empty($options) && is_array($options)){
+            foreach($options as $curlopt => $value){
+                curl_setopt($ch, $curlopt, $value);
+            }
+        }
+        
+        $result = curl_exec($ch);
+        if (curl_errno($ch)) {
+            return json_encode(['error' => curl_error($ch)]);
+        }
+        curl_close($ch);
+        return $result;
+    }
+    
+    public function delete($url, $data, $options = []){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ["X-HTTP-Method-Override: delete"]);
+        
+        if(!empty($options) && is_array($options)){
+            foreach($options as $curlopt => $value){
+                curl_setopt($ch, $curlopt, $value);
+            }
+        }
+        
+        $result = curl_exec($ch);
+        if (curl_errno($ch)) {
+            return json_encode(['error' => curl_error($ch)]);
+        }
+        curl_close($ch);
+        return $result;
+    }
+    
+    public function request($url, $method = 'get', $data = [], $options = []){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_TIMEOUT,30);
+        switch($method){
+            case 'post':
+                curl_setopt($ch, CURLOPT_POST, true);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+                break;
+            case 'put':
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+                curl_setopt($ch, CURLOPT_HTTPHEADER, ["X-HTTP-Method-Override: put"]);
+                break;
+            case 'delete':
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+                curl_setopt($ch, CURLOPT_HTTPHEADER, ["X-HTTP-Method-Override: delete"]);
+                break;
+            default: break;
+        }
+        
+        if(!empty($options) && is_array($options)){
+            foreach($options as $curlopt => $value){
+                curl_setopt($ch, $curlopt, $value);
+            }
+        }
+        
+        $result = curl_exec($ch);
+        if (curl_errno($ch)) {
+            return json_encode(['error' => curl_error($ch)]);
+        }
+        curl_close($ch);
+        return $result;
+    }
+    
 }
